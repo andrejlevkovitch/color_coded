d2a41b3863d2b6eb0ceb39fab11c8733231d5853
andrejlevkovitch <andrejlevkovitch@gmail.com>
2020-01-12 22:48:37 +0300

    fix problem with LLVM_ROOT_DIR in configure env script

--- include/env/impl.cmake.hpp
+++ include/env/impl.cmake.hpp
@@ -7,12 +7,12 @@ namespace color_coded
   {
     static char constexpr const * const pwd{ "${CMAKE_CURRENT_SOURCE_DIR}" };
     static char constexpr const * const clang_include
-    { "-isystem${LLVM_DIR}/include" };
+    { "-isystem${LLVM_ROOT_DIR}/include" };
     static char constexpr const * const clang_include_cpp
-    { "-isystem${LLVM_DIR}/include/c++/v1" };
+    { "-isystem${LLVM_ROOT_DIR}/include/c++/v1" };
     static char constexpr const * const clang_include_lib
-    { "-isystem${LLVM_DIR}/lib/clang/${CLANG_VERSION}/include" };
+    { "-isystem${LLVM_ROOT_DIR}/lib/clang/${CLANG_VERSION}/include" };
     static char constexpr const * const clang_resource_dir
-    { "-resource-dir=${LLVM_DIR}/lib/clang/${CLANG_VERSION}" };
+    { "-resource-dir=${LLVM_ROOT_DIR}/lib/clang/${CLANG_VERSION}" };
   };
 }
--- src/main.cpp
+++ src/main.cpp
@@ -66,7 +66,7 @@ namespace color_coded
 
   static int api_version(lua_State * const lua)
   {
-    std::size_t constexpr const version{ 0xeb82f9f };
+    std::size_t constexpr const version{ 0x60c2593 };
     lua_pushinteger(lua, version);
     return 1;
   }
